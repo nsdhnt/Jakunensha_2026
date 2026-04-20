8dad3d68f56c3504995b1a46f1e3482d8e388a4a
nsdhnt1012 <2250545@ecc.ac.jp>
2026-04-20 19:48:17 +0900

    提出

--- src/js/index.js
+++ src/js/index.js
@@ -45,16 +45,30 @@ const Songs = [
 
 // 参考用, 自由に使ってください
 // 不要な場合削除していただいて構いません。
-OutputLists.innerHTML = `
-    <li>
-        <picture>
-            <img src="img/no_img.png" alt="" />
-        </picture>
-        <div class="name_wrap">
-            <h2>SongName</h2>
-            <p>Artist</p>
-        </div>
-    </li >
-`;
+// OutputLists.innerHTML = `
+//     <li>
+//         <picture>
+//             <img src="img/no_img.png" alt="" />
+//         </picture>
+//         <div class="name_wrap">
+//             <h2>SongName</h2>
+//             <p>Artist</p>
+//         </div>
+//     </li >
+// `;
 
 // ここから下は自由に編集可
+Songs.forEach((song)=>{
+    console.log(song.songName);
+    OutputLists.innerHTML += `
+        <li>
+            <picture>
+                <img src="img/${song.imgPass}" alt="" />
+            </picture>
+            <div class="name_wrap">
+                <h2>${song.SongName}</h2>
+                <p>${song.artistName}</p>
+            </div>
+        </li >
+    `
+})
